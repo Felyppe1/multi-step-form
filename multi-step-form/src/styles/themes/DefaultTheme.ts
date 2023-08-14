3e743170bae62246c7fbfa8a5cdedee9e0e777ea
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-08-13 22:23:09 -0300

    feat: icons and theme

---
+++ multi-step-form/src/styles/themes/DefaultTheme.ts
@@ -0,0 +1,12 @@
+export const DefaultTheme = {
+    bgColor: '#EEF5FF',
+    bgActiveChoices: '#F8F9FE',
+    bgButton: '#03295A',
+    bgActiveStep: '#BDE2FC',
+    bgActiveCheckbox: '#433DF6',
+    hoverButton: '#938CFD',
+    addsOnPriceText: '#7971C3',
+    totalPriceText: '#4E41DB',
+    subText: '#A2A2AA',
+    errorText: '#D5374F',
+}
